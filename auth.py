66eafb06f46bb11bd8dcda8c9c23d2b9b2e5165b
user <user@users-iMac.local>
2026-01-03 08:51:08 -0700

    Add credentials

--- auth.py
+++ auth.py
@@ -1,22 +1,12 @@
-"""
-TicketDrop 2.0 - Authentication
-Uses Streamlit secrets for credentials
-"""
-
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
 def get_google_client():
-    creds_dict = dict(st.secrets["gcp_service_account"])
-    creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+    creds = Credentials.from_service_account_file('service_account.json', scopes=SCOPES)
     return gspread.authorize(creds)
 
 def get_spreadsheet():
-    client = get_google_client()
-    return client.open("Rick's TicketDrop 2.0")
+    return get_google_client().open("Rick's TicketDrop 2.0")
